ccd0a470db1746fa011ce559e12abf4ca1671f4a
BLUMSGKT-PC\jhlee <jiahaolee2112@gmail.com>
2025-08-27 14:44:19 +0800

    add echo function

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Groot {
     public static void iAmGroot(){
         System.out.print("I am Groot. ");
@@ -5,13 +7,28 @@ public class Groot {
     public static void greet(){
         iAmGroot();
         System.out.println("(Hello. What can I do for you?)");
-        System.out.println();
+    }
+    public static void echo(String command){
+        iAmGroot();
+        System.out.println("("+command+")");
+
     }
     public static void exit(){
         System.out.println("We are Groot. (Goodbye.)");
     }
     public static void main(String[] args) {
         greet();
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit){
+            String command = input.nextLine();
+            if (command.equals("bye")){
+                exit = true;
+            }
+            else{
+                echo(command);
+            }
+        }
         exit();
     }
 }
