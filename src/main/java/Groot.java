f70e023112ce508cc51e8300e3c6b372995cdc1f
BLUMSGKT-PC\jhlee <jiahaolee2112@gmail.com>
2025-08-29 20:48:56 +0800

    add list and add function

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,32 +1,51 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Groot {
-    public static void iAmGroot(){
+    private static final ArrayList<String> list = new ArrayList<String>();
+
+    public static void iAmGroot() {
         System.out.print("I am Groot. ");
     }
-    public static void greet(){
+
+    public static void greet() {
         iAmGroot();
         System.out.println("(Hello. What can I do for you?)");
     }
-    public static void echo(String command){
-        iAmGroot();
-        System.out.println("("+command+")");
 
+    public static void echo(String text) {
+        iAmGroot();
+        System.out.println("(" + text + ")");
     }
-    public static void exit(){
+
+    public static void exit() {
         System.out.println("We are Groot. (Goodbye.)");
     }
+
+    public static void add(String text) {
+        list.add(text);
+        echo("added: " + text);
+    }
+
+    public static void displayList() {
+        for (int i = 0; i < list.size(); i++) {
+            String listItem = Integer.toString(i + 1) + ": " + list.get(i);
+            echo(listItem);
+        }
+    }
+
     public static void main(String[] args) {
         greet();
         Scanner input = new Scanner(System.in);
         boolean exit = false;
-        while (!exit){
-            String command = input.nextLine();
-            if (command.equals("bye")){
+        while (!exit) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
                 exit = true;
-            }
-            else{
-                echo(command);
+            } else if (userInput.equals("list")) {
+                displayList();
+            } else {
+                add(userInput);
             }
         }
         exit();
